2102c18952b2f4e9fe7ddd03a3b3a65fef951fd4
Piotr <pioula111@gmail.com>
2024-09-01 15:14:24 +0200

    object mapper

--- src/main/java/com/pu429640/config/UserTagStorageConfig.java
+++ src/main/java/com/pu429640/config/UserTagStorageConfig.java
@@ -1,5 +1,6 @@
 package com.pu429640.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pu429640.services.IUserTagStorage;
 import com.pu429640.services.UserTagLocalStorage;
 import com.pu429640.services.UserTagRedisStorage;
@@ -20,20 +21,24 @@ public class UserTagStorageConfig {
     private String storageType;
 
     @Bean
-    public IUserTagStorage userTagStorage(RedisConnectionFactory redisConnectionFactory) {
+    public IUserTagStorage userTagStorage(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
         if ("redis".equalsIgnoreCase(storageType)) {
-            return new UserTagRedisStorage(redisTemplate(redisConnectionFactory));
+            return new UserTagRedisStorage(redisTemplate(redisConnectionFactory, objectMapper));
         } else {
             return new UserTagLocalStorage();
         }
     }
 
     @Bean
-    public RedisTemplate<String, UserTagEvent> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, UserTagEvent> redisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
         RedisTemplate<String, UserTagEvent> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(UserTagEvent.class));
+        
+        Jackson2JsonRedisSerializer<UserTagEvent> serializer = new Jackson2JsonRedisSerializer<>(UserTagEvent.class);
+        serializer.setObjectMapper(objectMapper);
+        
+        template.setValueSerializer(serializer);
         return template;
     }
 }
