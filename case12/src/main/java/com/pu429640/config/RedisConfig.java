0477899781a608d2dac50e0618d7918a8ba2cc2b
Piotr <pioula111@gmail.com>
2024-09-05 11:37:43 +0200

    redistemplate fix

--- case12/src/main/java/com/pu429640/config/RedisConfig.java
+++ case12/src/main/java/com/pu429640/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.pu429640.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pu429640.domain.UserTagEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,8 @@ import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.List;
 
@@ -23,9 +27,15 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+    public RedisTemplate<String, UserTagEvent> redisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        RedisTemplate<String, UserTagEvent> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+
+        Jackson2JsonRedisSerializer<UserTagEvent> serializer = new Jackson2JsonRedisSerializer<>(UserTagEvent.class);
+        serializer.setObjectMapper(objectMapper);
+
+        template.setValueSerializer(serializer);
         return template;
     }
 }
