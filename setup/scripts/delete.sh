fe4a664a3cebd278b98fbf1f5c245122d7f1c136
Piotr <pioula111@gmail.com>
2024-09-07 15:54:10 +0200

    fixed the redis.sh

--- setup/scripts/delete.sh
+++ setup/scripts/delete.sh
@@ -1,5 +1,4 @@
 kubectl delete -f ~/k8s-manifests/redis/redis-cluster.yml
 kubectl delete pvc data-redis-cluster-0 data-redis-cluster-1 data-redis-cluster-2 data-redis-cluster-3 data-redis-cluster-4 data-redis-cluster-5
 kubectl delete -f ~/k8s-manifests/redis/cm.yml
-kubectl delete -f ~/k8s-manifests/local-path/local-path.yml
 kubectl delete -f ~/k8s-manifests/namespace/namespace.yml
