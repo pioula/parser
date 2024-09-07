fa164bf3d7525a7f9a45e003aeccb9bb91c4e349
Piotr <pioula111@gmail.com>
2024-09-07 13:51:23 +0200

    creating resources from ansible node

--- setup/scripts/setup.sh
+++ setup/scripts/setup.sh
@@ -10,9 +10,24 @@ read -p "Enter your username: " username
 read -s -p "Enter your password: " password
 echo
 
+#Validate install:
+kubectl version
+
 echo "Installing Ansible"
-sudo apt update
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y python3-pip
 sudo apt -y install ansible sshpass
+
+#Download Kubernetes Tools using Curl:
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+#Verify Checksum (Response should be kubectl:OK): 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+#Install Kubernetes Tools: 
+sudo install -y -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Use the provided username and password in the loop
 for i in $(seq -w 01 10); do
@@ -37,5 +52,8 @@ ansible-playbook --extra-vars "ansible_user=$username ansible_password=$password
 echo "Configuration of the cluster complete!"
 echo "Creating kubernetes resources:"
 cp -r ../k8s-manifests ~/
+
+mkdir ~/.kube/config
+sudo scp "$username@${username}vm110.rtb-lab.pl:/etc/kubernetes/admin.conf" ~/.kube/config
 
 ansible-playbook --extra-vars "ansible_user=$username ansible_password=$password" ~/ansible/playbook/create_k8s_resources.yml -i ~/ansible/inventory/kube_inventory
